2db1e0794e515486ccbcb4b8f33ab60c09ba7b4e
jhs512 <jangka512@gmail.com>
2026-06-15 10:21:03 +0900

    019

--- src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
@@ -43,11 +43,6 @@ public class ApiV1MemberController {
     public RsData<MemberDto> join(
             @RequestBody @Valid MemberJoinReqBody reqBody
     ) {
-        memberService.findByUsername(reqBody.username)
-                .ifPresent(_ -> {
-                    throw new ServiceException("409-1", "이미 존재하는 아이디입니다.");
-                });
-
         Member member = memberService.join(
                 reqBody.username(),
                 reqBody.password(),
