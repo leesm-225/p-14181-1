f07f6a767324b43a84f490ca3849e0e07e9d1b39
jhs512 <jangka512@gmail.com>
2026-06-17 14:09:49 +0900

    071

--- src/test/java/com/back/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/back/domain/member/member/service/AuthTokenServiceTest.java
@@ -80,12 +80,14 @@ public class AuthTokenServiceTest {
     }
 
     @Test
-    @DisplayName("Ut.jwt.toString 를 통해서 JWT 생성, {name=\"Paul\", age=23}, 그리고 jwt 유효성 체크")
+    @DisplayName("Ut.jwt.toString 를 통해서 JWT 생성, {name=\"Paul\", age=23}, 그리고 jwt 유효성 체크, 그리고 payload 추출")
     void t3() {
+        Map<String, Object> payload = Map.of("name", "Paul", "age", 23);
+
         String jwt = Ut.jwt.toString(
                 secret,
                 expireSeconds,
-                Map.of("name", "Paul", "age", 23)
+                payload
         );
 
         assertThat(jwt).isNotBlank();
@@ -94,6 +96,10 @@ public class AuthTokenServiceTest {
                 Ut.jwt.isValid(secret, jwt)
         )
                 .isTrue();
+
+        Map<String, Object> parsedPayload = Ut.jwt.payload(secret, jwt);
+
+        assertThat(parsedPayload).containsAllEntriesOf(payload);
     }
 
     @Test
