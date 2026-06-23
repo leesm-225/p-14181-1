d9b106eae409971eb3ec333fa40a8c35d9786435
leesm-225 <lshine12393@gmail.com>
2026-06-23 10:13:58 +0900

    128

--- src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
@@ -8,6 +8,7 @@ import com.back.domain.member.member.service.MemberService;
 import com.back.global.exception.ServiceException;
 import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -42,6 +43,7 @@ public class ApiV1MemberController {
 
     @PostMapping
     @Transactional
+    @Operation(summary = "가입")
     public RsData<MemberDto> join(
             @RequestBody @Valid MemberJoinReqBody reqBody
     ) {
@@ -77,6 +79,8 @@ public class ApiV1MemberController {
     }
 
     @PostMapping("/login")
+    @Transactional(readOnly = true)
+    @Operation(summary = "로그인")
     public RsData<MemberLoginResBody> login(
             @RequestBody @Valid MemberLoginReqBody reqBody
     ) {
@@ -106,6 +110,7 @@ public class ApiV1MemberController {
 
 
     @DeleteMapping("/logout")
+    @Operation(summary = "로그아웃")
     public RsData<Void> logout() {
         rq.deleteCookie("apiKey");
         rq.deleteCookie("accessToken");
@@ -118,6 +123,8 @@ public class ApiV1MemberController {
 
 
     @GetMapping("/me")
+    @Transactional(readOnly = true)
+    @Operation(summary = "내 정보")
     public MemberWithUsernameDto me() {
         Member actor = memberService.findById(rq.getActor().getId()).get();
 
--- src/main/java/com/back/domain/post/post/controller/ApiV1AdmPostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1AdmPostController.java
@@ -1,9 +1,11 @@
 package com.back.domain.post.post.controller;
 
 import com.back.domain.post.post.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,8 @@ public class ApiV1AdmPostController {
     }
 
     @GetMapping("/count")
+    @Transactional(readOnly = true)
+    @Operation(summary = "전체 글 개수")
     public AdmPostCountResBody count() {
         return new AdmPostCountResBody(
                 postService.count()
